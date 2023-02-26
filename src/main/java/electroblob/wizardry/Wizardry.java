6f66e3663ed6d7b117189ebe837a6c96117573a2
Archi_DreamZ <jcph686@163.com>
2023-02-26 16:47:29 +0800

    Revert "Revert "Revert "上传更新，包含错误修复与大量法术数据调整"""
    This reverts commit 8a45358645d4d5950361958146fdae33c889f1ac.

--- src/main/java/electroblob/wizardry/Wizardry.java
+++ src/main/java/electroblob/wizardry/Wizardry.java
@@ -94,7 +94,7 @@ public class Wizardry {
 
 	public static final String MODID = "wizardry";
 	public static final String NAME = "Electroblob's Wizardry";
-	public static final String VERSION = "1.1.6-2";
+	public static final String VERSION = "1.1.5";
 
 	// Constants
 	/** The amount of mana each magic crystal is worth */
